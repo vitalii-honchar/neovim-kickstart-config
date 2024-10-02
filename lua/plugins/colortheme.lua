44441bbae8b1da9e2b060bfbd29ddd091e1cabc6
Vitalii Honchar <weaxme@gmail.com>
2024-10-02 07:32:01 -0400

    Changed colorscheme to catppuccin-mocha

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'catppuccin-mocha'
 
     vim.cmd.hi 'Comment gui=none'
   end,
